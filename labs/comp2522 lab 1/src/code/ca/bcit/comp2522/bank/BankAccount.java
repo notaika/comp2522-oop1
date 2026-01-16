f61221cdb64b01821417e4dab2cd5ebbd28880c7
aika <ercamanalo@gmail.com>
2026-01-16 00:55:25 -0800

    backup

--- labs/comp2522 lab 1/src/code/ca/bcit/comp2522/bank/BankAccount.java
+++ labs/comp2522 lab 1/src/code/ca/bcit/comp2522/bank/BankAccount.java
@@ -1,4 +1,12 @@
 package ca.bcit.comp2522.bank;
 
+/**
+ * Represents a Bank Account.
+ *
+ * @author Aika Manalo - Set 2C
+ * @author Thor Baker - Set 2C
+ * @version 1.0
+ */
 public class BankAccount {
+
 }
