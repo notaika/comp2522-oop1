f772e7475b5f0b2a87e76b813e778da8fd140cd8
aika <ercamanalo@gmail.com>
2026-01-21 22:03:17 -0800

    finished Creature class logic

--- labs/comp2522 lab 2/src/code/ca/bcit/comp2522/lab2/creatures/Creature.java
+++ labs/comp2522 lab 2/src/code/ca/bcit/comp2522/lab2/creatures/Creature.java
@@ -15,6 +15,13 @@ public class Creature
     private final static int YEAR_UPPER_LIMIT = 2026;
     private final static int MONTH_UPPER_LIMIT = 1;
     private final static int DAY_UPPER_LIMIT = 21;
+    private final static Date DATE_UPPER_LIMIT;
+
+    // Initialize upper date limit??? <- is this the right way to do this???
+    static
+    {
+        DATE_UPPER_LIMIT = new Date(YEAR_UPPER_LIMIT, MONTH_UPPER_LIMIT, DAY_UPPER_LIMIT);
+    }
 
     private final String name; // not null or empty
     private final Date dateOfBirth; // must not be in the future
@@ -34,10 +41,42 @@ public class Creature
         this.health = health;
     }
 
+    /**
+     * Returns the creatures name.
+     *
+     * @return creatures name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Returns the creatures date of birth.
+     *
+     * @return creature's date of birth
+     */
+    public Date getDateOfBirth()
+    {
+        return dateOfBirth;
+    }
+
+    /**
+     * Returns the creature's health.
+     *
+     * @return creature's health
+     */
+    public int getHealth()
+    {
+        return health;
+    }
+
     /*
+     * Checks if health is between 0 and 100.
+     * Throws IllegalArgumentException if invalid.
      *
-     *
-     * @param healthToCheck
+     * @param healthToCheck health amount to check
+     * @throws IllegalArgumentException exception thrown if health is less than 0 or greater than 100
      */
     private static void validateHealth(int healthToCheck)
     {
@@ -49,15 +88,14 @@ public class Creature
     }
 
     /*
+     * Checks if date of birth is not in the future.
+     * Throws IllegalArgumentException if invalid.
      *
-     *
-     * @param dateToCheck
+     * @param dateToCheck date of birth to check
+     * @throws IllegalArgumentException exception thrown if year, month or day is greater than the upper limit
      */
     private static void validateDOB(Date dateToCheck)
     {
-        final Date DATE_UPPER_LIMIT;
-        DATE_UPPER_LIMIT = new Date(YEAR_UPPER_LIMIT, MONTH_UPPER_LIMIT, DAY_UPPER_LIMIT);
-
         if  (dateToCheck.getYear() > YEAR_UPPER_LIMIT ||
             (dateToCheck.getYear() == YEAR_UPPER_LIMIT && dateToCheck.getMonth() > MONTH_UPPER_LIMIT) ||
             (dateToCheck.getYear() == YEAR_UPPER_LIMIT && dateToCheck.getMonth() == MONTH_UPPER_LIMIT && dateToCheck.getDay() > DAY_UPPER_LIMIT))
@@ -68,10 +106,11 @@ public class Creature
     }
 
     /*
-     *
+     * Checks if name is null or empty.
+     * Throw IllegalArgumentException if invalid.
      *
      * @param nameToCheck
-     * @throws IllegalArgumentException
+     * @throws IllegalArgumentException exception thrown if name is null or empty.
      */
     private static void validateName(String nameToCheck)
     {
@@ -83,6 +122,7 @@ public class Creature
 
     /**
      * Checks if creature is still alive.
+     * Returns true if health is greater than 0.
      *
      * @param healthToCheck health to check
      * @return true if healthToCheck is greater than min health
@@ -93,8 +133,8 @@ public class Creature
     }
 
     /**
-     * Reduces creature's health by the specified damage amount.
-     * If health falls below the stated minimum, resets to minimum.
+     * Reduces health by damage. If health goes below 0, set it to 0.
+     * If damage is negative, throw an unchecked DamageException.
      *
      * @param damage amount of health to take away (damage), must not go below stated minimum
      * @throws DamageException exception thrown if damage is below the stated minimum
@@ -108,16 +148,23 @@ public class Creature
 
         health -= damage;
 
+        System.out.println("Damage taken: " + damage + " points");
+
         // If health goes below min allowed, reset it to min.
         if (health < MIN_HEALTH)
         {
             health = MIN_HEALTH;
+            System.out.println("Health: " + getHealth());
+            System.out.println(getName() + " is dead.");
+        } else
+        {
+            System.out.println("Health: " + getHealth());
         }
     }
 
     /**
-     * Add's specified heal amount to creature's health.
-     * Health cannot exceed stated maximum amount.
+     * Increases health by healAmount but cannot exceed 100.
+     * If healing amount is negative, throw an unchecked HealingException.
      *
      * @param healAmount amount of health to add (heal), must not go below minimum
      * @throws HealingException exception throw if heal amount is below the allowed minimum
@@ -131,24 +178,62 @@ public class Creature
 
         health += healAmount;
 
+        System.out.println("Healed for: " + healAmount + " points");
+
         // If health goes below max allowed, reset it to max.
         if (health > MAX_HEALTH)
         {
             health = MAX_HEALTH;
+            System.out.println("Health: " + getHealth());
+            System.out.println("Cannot heal above " + MAX_HEALTH);
+        } else
+        {
+            System.out.println("Health: " + getHealth());
         }
     }
 
     /**
-     * Calculates
+     * Calculates the creature's age in years based on its date of birth.
      *
      * @return age in years
      */
     public int getAgeYears()
     {
-
+        // Subtract creatures year DOB from upper limit year
+        if ((dateOfBirth.getMonth() > MONTH_UPPER_LIMIT) ||
+            (dateOfBirth.getMonth() == MONTH_UPPER_LIMIT && dateOfBirth.getDay() > DAY_UPPER_LIMIT))
+        {
+            return DATE_UPPER_LIMIT.getYear() - dateOfBirth.getYear() - 1;
+        }
+        return DATE_UPPER_LIMIT.getYear() - dateOfBirth.getYear();
     }
 
+    /**
+     * Prints the creature's name, dateOfBirth, age in years, and health.
+     */
+    public void getDetails()
+    {
+        System.out.println("Name: " + getName() +
+                           "\nDate of Birth: " + dateOfBirth.getYyyyMmDd() +
+                           "\nAge (in years): " + getAgeYears() +
+                           "\nHealth: " + getHealth());
+    }
 
+    public static void main(String[] args)
+    {
+        final Date testDOB = new Date(2007, 10, 7);
+
+        final Creature testCreature = new Creature("Mischa", testDOB, 99);
+
+        System.out.println("=== THE LORAX CREATURE DETAILS ===");
+        testCreature.getDetails();
+
+
+
+        testCreature.takeDamage(20);
+
+        testCreature.takeDamage(100);
+    }
 }
 
 
