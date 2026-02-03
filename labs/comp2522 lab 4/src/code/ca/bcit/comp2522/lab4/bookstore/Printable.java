1b555bca2b8cea6476ad6a86e1956a62c549ef58
aika <ercamanalo@gmail.com>
2026-02-03 15:50:32 -0800

    feat: started lab 4. finished Printable and Reversible interfaces

--- labs/comp2522 lab 4/src/code/ca/bcit/comp2522/lab4/bookstore/Printable.java
+++ labs/comp2522 lab 4/src/code/ca/bcit/comp2522/lab4/bookstore/Printable.java
@@ -1,5 +1,22 @@
 package ca.bcit.comp2522.lab4.bookstore;
 
-public class Printable
+/**
+ * Defines the contract for objects that can display their internal state.
+ * <p>
+ * Implementing classes must provide a body to print all of their instance
+ * variables (including those inherited from parent classes) in a sentence.
+ * </p>
+ *
+ * @author Aika Manalo - Set 2C
+ * @author Devan Lam - Set 2C
+ *
+ * @version 1.0
+ */
+public interface Printable
 {
+    /**
+     * Prints every instance variable (including those in parent classes)
+     * of an object in a sentence.
+     */
+    void display();
 }
--- labs/comp2522 lab 4/src/code/ca/bcit/comp2522/lab4/bookstore/Reversible.java
+++ labs/comp2522 lab 4/src/code/ca/bcit/comp2522/lab4/bookstore/Reversible.java
@@ -1,5 +1,25 @@
 package ca.bcit.comp2522.lab4.bookstore;
 
-public class Reversible
+/**
+ * Defines the contract for objects that can be displayed in reverse.
+ * <p>
+ * Implementing classes must provide the body to print the identifier
+ * in backward order
+ * </p>
+ *
+ * @author Aika Manalo - Set 2C
+ * @author Devan Lam - Set 2C
+ *
+ * @version 1.0
+ */
+public interface Reversible
 {
+    /**
+     * Prints the name or title of an object backwards.
+     * <p>
+     * e.g. Given an instance variable named "Java" it
+     * will be printed as "avaJ".
+     * </p>
+     */
+    void backward();
 }
