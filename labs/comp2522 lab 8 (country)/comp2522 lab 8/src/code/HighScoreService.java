9242d452f98b51cf8d84a37426119638d5e95123
aika <ercamanalo@gmail.com>
2026-03-23 23:41:40 -0700

    some lab 8 logic

--- labs/comp2522 lab 8 (country)/comp2522 lab 8/src/code/HighScoreService.java
+++ labs/comp2522 lab 8 (country)/comp2522 lab 8/src/code/HighScoreService.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
@@ -8,22 +14,83 @@ import java.nio.file.Path;
  */
 public class HighScoreService
 {
-    private static final int SCORE_THRESHOLD = Integer.MAX_VALUE;
+    private static final int NO_BEST = Integer.MAX_VALUE;
+    private static final Charset UTF8 = StandardCharsets.UTF_8;
+    private static final String PREFIX = "COUNTRY=";
 
     // keeps track of highest score
     // reads highscore.txt
     // overwrites it if highest score
 
-
-    private final int highestScore;
+    private int highestScore; // lowest number of attempts
 
     public HighScoreService(final Path path)
     {
-        
+        this.highestScore = NO_BEST;
+        readHighScore(path);
     }
 
-    public static void main(String[] args)
+    private static void validatePath(final Path pathToCheck)
     {
+        if (pathToCheck == null)
+        {
+            throw new IllegalArgumentException("Invalid path");
+        }
 
+
+    }
+
+    private void readHighScore(final Path path)
+    {
+        if (Files.notExists(path))
+        {
+            return;
+        }
+
+        try (BufferedReader reader = Files.newBufferedReader(path,
+                                                             UTF8))
+        {
+            final String line = reader.readLine();
+
+            if (line != null && line.startsWith(PREFIX))
+            {
+                final String parsedScore = line.substring(PREFIX.length())
+                                               .trim();
+                this.highestScore = Integer.parseInt(parsedScore);
+            }
+        }
+        catch (final IOException e)
+        {
+            System.err.println("File error: " + e.getMessage());
+        }
+        catch (final NumberFormatException e)
+        {
+            System.err.println("Parsing error: " + e.getMessage());
+        }
+    }
+
+    public boolean overwriteFile(final Path path,
+                                 final int attempts)
+    {
+        if (attempts < highestScore)
+        {
+            highestScore = attempts;
+            writeHighScore(path);
+            return true;
+        }
+        return false;
+    }
+
+    private void writeHighScore(final Path path)
+    {
+        try (BufferedWriter writer = Files.newBufferedWriter(path,
+                                                             UTF8))
+        {
+            writer.write(PREFIX + highestScore);
+        }
+        catch (final IOException e)
+        {
+            System.err.println("Could not save high score " + e.getMessage());
+        }
     }
 }
--- labs/comp2522 lab 8 (country)/comp2522 lab 8/src/code/LoggerService.java
+++ labs/comp2522 lab 8 (country)/comp2522 lab 8/src/code/LoggerService.java
@@ -1,3 +1,13 @@
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Updates and keeps track of game logs.
+ *
+ * @author Aika Manalo - Set 2C
+ * @version 1.0
+ */
 public class LoggerService
 {
+    private static final Charset UTF8 = StandardCharsets.UTF_8;
 }
