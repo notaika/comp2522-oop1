11f525098843552a2de0d6587aa87a23ca791c28
aika <ercamanalo@gmail.com>
2026-04-09 18:41:47 -0700

    feat: lab 10 completed

--- labs/comp2522 lab 10/comp2522 lab 10/src/code/ca/bcit/comp2522/lab10/quiz/Question.java
+++ labs/comp2522 lab 10/comp2522 lab 10/src/code/ca/bcit/comp2522/lab10/quiz/Question.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+package ca.bcit.comp2522.lab10.quiz;
 
-public class Question {
+public class Question
+{
+    private final String question;
+    private final String answer;
+
+    public Question(String question,
+                    String answer)
+    {
+        this.question = question;
+        this.answer   = answer;
+    }
+
+    public String getQuestion()
+    {
+        return question;
+    }
+
+    public String getAnswer()
+    {
+        return answer;
+    }
+
+    public boolean isCorrect(final String answerToCheck)
+    {
+        return this.answer.equalsIgnoreCase(answerToCheck.trim());
+    }
 }
--- labs/comp2522 lab 10/comp2522 lab 10/src/code/ca/bcit/comp2522/lab10/quiz/QuizApp.java
+++ labs/comp2522 lab 10/comp2522 lab 10/src/code/ca/bcit/comp2522/lab10/quiz/QuizApp.java
@@ -1,5 +1,14 @@
+package ca.bcit.comp2522.lab10.quiz;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * A quizzing game application.
@@ -11,6 +20,130 @@ import java.util.Map;
  */
 public class QuizApp
 {
-    private final String name;
-    private final Map<String, >
+    private static final int MAX_QUESTIONS_PER_GAME = 10;
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
+
+    // All possible questions
+    private final List<Question> questionBank;
+
+    // Current session?
+    private final List<Question> questionsSeen;
+    private final List<Question> questionsWrong;
+    private int score;
+    private int currQuestionCount;
+
+    public QuizApp()
+    {
+        this.questionBank = new ArrayList<>();
+        this.questionsSeen = new ArrayList<>();
+        this.questionsWrong = new ArrayList<>();
+        this.score = 0;
+        this.currQuestionCount = 0;
+    }
+
+    public List<Question> getQuestionBank()
+    {
+        return questionBank;
+    }
+
+    public List<Question> getQuestionsSeen()
+    {
+        return questionsSeen;
+    }
+
+    public List<Question> getQuestionsWrong()
+    {
+        return questionsWrong;
+    }
+
+    public int getScore()
+    {
+        return score;
+    }
+
+    public int getCurrQuestionCount()
+    {
+        return currQuestionCount;
+    }
+
+    public void loadQuestions(final Path path) throws IOException
+    {
+        try (final BufferedReader reader = Files.newBufferedReader(path,
+                                                                   CHARSET))
+        {
+            String line;
+            while((line = reader.readLine()) != null)
+            {
+                if (!line.trim()
+                         .isEmpty())
+                {
+                    final Question newQuestion = createQuestion(line);
+
+                    if (newQuestion != null)
+                    {
+                        questionBank.add(newQuestion);
+                    }
+                }
+            }
+        }
+    }
+
+    public Question createQuestion(final String line)
+    {
+        final String[] parts = line.split("\\|");
+
+        final String prompt = parts[0].trim();
+        final String answer = parts[1].trim();
+
+        return new Question(prompt, answer);
+    }
+
+    public void startGame()
+    {
+        this.score = 0;
+        this.currQuestionCount = 0;
+        this.questionsWrong.clear();
+        this.questionsSeen.clear();
+
+        Collections.shuffle(questionBank);
+
+        // Grab 10 questions
+        for (int i = 0; i < MAX_QUESTIONS_PER_GAME; i++)
+        {
+            this.questionsSeen.add(questionBank.get(i));
+        }
+    }
+
+    public Question getCurrQuestion()
+    {
+        if (this.currQuestionCount < questionsSeen.size())
+        {
+            return questionsSeen.get(currQuestionCount);
+        }
+
+        return null;
+    }
+
+    public void submitAnswer(final String userAnswer)
+    {
+        Question currQuestion = getCurrQuestion();
+
+        if (currQuestion != null)
+        {
+            if (currQuestion.isCorrect(userAnswer))
+            {
+                score++;
+            } else
+            {
+                questionsWrong.add(currQuestion);
+            }
+
+            currQuestionCount++;
+        }
+    }
+
+    public int getTotalSessionQuestions()
+    {
+        return questionsSeen.size();
+    }
 }
