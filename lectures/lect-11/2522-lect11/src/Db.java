20dfbe4fe940677543abb7a8aa8dcc69701c03a2
aika <ercamanalo@gmail.com>
2026-04-09 18:41:16 -0700

    feat/fix: design patterns lecture and moved lecture files

--- lectures/lect-11/2522-lect11/src/Db.java
+++ lectures/lect-11/2522-lect11/src/Db.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+class Db
+{
+    private static Db db;
 
-public class Db {
+    // this constructor can't be called outside of this class because it's private
+    private Db()
+    {
+
+    }
+
+    // this can call the constructor
+    // why is it static?
+    // static means that theres ONE PER CLASS (shared by all instances of the whole class)
+    static Db getInstance()
+    {
+        // if and only if you don't have one..
+        if (db == null)
+        {
+            db = new Db(); // make one
+        }
+
+        return db;
+    }
 }
--- lectures/lect-11/2522-lect11/src/Main.java
+++ lectures/lect-11/2522-lect11/src/Main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class Main
+{
+    public static void main(final String[] args)
+    {
+        final Db db1;
+        final Db db2;
+        final Db db3;
 
-public class main {
+        // won't work anymore due to private constructor
+//        db1 = new Db();
+//        db2 = new Db();
+//        db3 = new Db();
+
+        // we use .getInstance instead
+        // these all reference to the same thing
+        db1 = Db.getInstance();
+        db2 = Db.getInstance();
+        db3 = Db.getInstance();
+
+        System.out.println(db1);
+        System.out.println(db2);
+        System.out.println(db3);
+    }
 }
